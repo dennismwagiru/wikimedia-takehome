de3d0b40dd8e3e265bf0892bf4c30b62038dd7cd
Nicholas Ray <nray@wikimedia.org>
2022-10-28 13:14:22 -0600

    Revise for frontend-focused people
    * Corrects the endpoint in the main.js instructions to use
    ?prefixsearch={search} instead of ?prefixtitle={search}
    
    * Changes api.php to allow for case-insensitive auto-completes
    
    * Disables process timeout in composer.json (I was having issues with the server
    stopping after 300 seconds which ate up time)
    
    * Removes jQuery and explicitly states to not use any third-party libraries.
    
    * Adds additional TODOs to main.js
    
    * Adds a design-spec folder with three images for how the design should look.
    
    * Adds styles.css and adds a TODO to make the design look like the design spec
    
    * Changes the HTML so that the base layout is easier to see and the word count
    is easier to see (before the word count was hidden)
    
    * Converts form submit button to link and adds code in main.js to enable it to work
    
    * Converts todos to be labeled so that a specific subset can be assigned.
    
    * Modifies README to reflect the changes above/adds accessibility as a
    consideration.

--- api.php
+++ api.php
@@ -2,14 +2,13 @@
 
 use App\App;
 
-// TODO: Improve the readability of this file through refactoring and documentation.
+// TODO A: Improve the readability of this file through refactoring and documentation.
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 $app = new App();
-// TODO: It would be nice to clean up the following code so that it's
-// easier to see the different routes and handlers for the API, and
-// simpler to add new ones.
+// TODO B: Clean up the following code so that it's easier to see the different
+// routes and handlers for the API, and simpler to add new ones.
 header( 'Content-Type: application/json' );
 if ( !isset( $_GET['title'] ) && !isset( $_GET['prefixsearch'] ) ) {
 	echo json_encode( [ 'content' => $app->getListOfArticles() ] );
@@ -17,7 +16,7 @@ if ( !isset( $_GET['title'] ) && !isset( $_GET['prefixsearch'] ) ) {
 	$list = $app->getListOfArticles();
 	$ma = [];
 	foreach ( $list as $ar ) {
-		if ( strpos( $ar, $_GET['prefixsearch'] ) !== false ) {
+		if ( strpos( strtolower( $ar ), strtolower( $_GET['prefixsearch'] ) ) !== false ) {
 			$ma[] = $ar;
 		}
 	}
--- main.js
+++ main.js
@@ -1,16 +1,42 @@
 'use strict';
 
 ( function () {
-	// NOTE:
-	// You may replace this file with Vue / React / Svelte / JS library of your choice,
-	// if you prefer, but that is definitely not required!
-	$('a').on('click', function (e) {
-		// TODO: When clicking the article title, we'd like to get the article text
-		//  and show it in the page without requiring a refresh of the page.
-	});
+	// NOTE: Please do not use any third-party libraries to implement the
+	// following as we want to keep the JS payload as small as possible. You may
+	// use ES6. There is no need to support IE11.
+	// 
+	// TODO A: Improve the readability of this file through refactoring and
+	// documentation. Make any changes you think are necessary.
+	// 
+	// TODO B: When typing in the "title" field, we want to auto-complete based on
+	// article titles that already exist. You may use the
+	// api.php?prefixsearch={search} endpoint for auto-completion. To avoid
+	// hitting the server endpoint excessively, please also add JavaScript code
+	// that ensures at least 200ms has passed between requests. Check the
+	// `design-spec/auto-complete-hover.png` file for the design spec. 
+	// Also, you don't need to make the autocomplete list disappear when the input
+	// has lost focus in this TODO. That will be handled as part of TODO D.
+	//
+	// TODO C: When the user selects an item from the auto-complete list, we want
+	// the textarea to populate with that article's contents. You may use the
+	// api.php?title={title} endpoint to get the article's contents. Check the
+	// `design-spec/auto-complete-select.png` file for the design spec.
+	//
+	// TODO D: The autocomplete list should only be shown when the input receives
+	// focus. The list should be hidden after the user selects an item from the
+	// list or after the input loses focus.
 
-    // TODO: When typing in the "title" field, we want to auto-complete based on article titles
-	//  that already exist. You may use the api.php?prefixtitle={search} endpoint for
-	// auto-completion. You can use https://api.jqueryui.com/autocomplete/ or another auto-complete
-	// widget of your choice.
+	function getFormButtonToWork() {
+		const submitButton = document.querySelector( '.submit-button' );
+		const form = document.querySelector( 'form' );
+
+		// Make form submit button work when submit is clicked.
+		submitButton.addEventListener( 'click', (e) => {
+			e.preventDefault();
+			form.submit();
+		} );
+	}
+
+	// Waiting for page to load which takes about 1.5 seconds on my machine.
+	setTimeout( getFormButtonToWork, 1500);
 }() )
